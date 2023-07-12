6cc54f27a965759c760c415de93e1db9c480f5af
Sandru Gabriel <gabi200611@yahoo.com>
2023-07-12 12:46:50 +0300

    done

---
+++ AplicatieBiblioteca/Controllers/ControllerImprumuturi.cs
@@ -0,0 +1,42 @@
+﻿using AplicatieBiblioteca.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicatieBiblioteca.Controllers
+{
+    internal class ControllerImprumuturi
+    {
+
+        private List<Imprumut> imprumuturi;
+
+        public ControllerImprumuturi() { 
+        
+            imprumuturi = new List<Imprumut>();
+
+            load();
+
+        }
+
+        public void load()
+        {
+
+            string path = Application.StartupPath + @"/data/imprumuturi.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            string t;
+
+            while ((t = streamReader.ReadLine()) != null)
+            {
+                Imprumut imprumut = new Imprumut(t);
+                imprumuturi.Add(imprumut);
+            }
+            streamReader.Close();
+        }
+
+    }
+}
